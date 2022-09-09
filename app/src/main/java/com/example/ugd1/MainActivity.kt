1c3e8093b9725bcc7bf422c3e4374181e2932e54
ASUS <drl.yk99@gmail.com>
2022-09-09 15:22:00 +0700

    UGD1

--- app/src/main/java/com/example/ugd1/MainActivity.kt
+++ app/src/main/java/com/example/ugd1/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.ugd1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputUsername: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+    private lateinit var mainLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTitle("User Login")
+
+        inputUsername = findViewById(R.id.inputLayoutUsername)
+        inputPassword = findViewById(R.id.inputLayoutPassword)
+        mainLayout = findViewById(R.id.mainLayout)
+        val btnRegister: Button = findViewById(R.id.btnRegister)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+            var checkLogin = false
+            val username: String = inputUsername.getEditText()?.getText().toString()
+            val password: String = inputPassword.getEditText()?.getText().toString()
+
+            if(username.isEmpty()){
+                inputUsername.setError("Username must be filled with text!")
+                checkLogin = false
+            }
+            if(password.isEmpty()){
+                inputPassword.setError("Password must be filled with text!")
+                checkLogin = false
+            }
+
+            if(username == "admin" && password == "admin") {
+                checkLogin = true
+            }
+            else if(username != "admin" || password != "admin"){
+                checkLogin = false
+                Snackbar.make(mainLayout, "Username atau Password salah!", Snackbar.LENGTH_LONG).show()
+            }
+            if(!checkLogin) return@OnClickListener
+
+//            val moveHome = Intent(this@MainActivity, HomeActivity::class.java)
+//            startActivity(moveHome)
+        })
     }
 }
